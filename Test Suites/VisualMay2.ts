525b330702b09f608858e2268473b5f0ddda89fc
om <om@DESKTOP-ULKJEBS>
2023-05-11 12:19:34 +0530

    commit collection

--- Test Suites/VisualMay2.ts
+++ Test Suites/VisualMay2.ts
@@ -11,4 +11,11 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>2463969d-efca-4ec0-b9c2-09a409f49f10</testSuiteGuid>
+   <testCaseLink>
+      <guid>65702eaa-c5ad-4a8c-be38-ffff90b9856a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Visual Testing_TCs/TC_01</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
